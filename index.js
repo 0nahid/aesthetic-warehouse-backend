0cea9cd7ceda9393c17c8190b080c8c87ed6b4df
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-25 06:27:10 +0600

    added middlewares

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500
+const cors = require('cors')
+// middleware
+app.use(cors())
+app.use(express.json())
+
 
 app.get('/', (req, res) => res.send('Hello from Aesthetic Ware House!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
