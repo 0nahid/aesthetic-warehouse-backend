e3187e6907bb42a8beea6a040d1b0522926079f6
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-25 06:21:53 +0600

    route msg updated

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello from Aesthetic Ware House!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
