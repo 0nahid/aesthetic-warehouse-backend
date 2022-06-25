2a841df5cd7aa2cd231ad468b1e69b2d9fed7915
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-25 06:32:42 +0600

    connected to mongoDB

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+// Connect to MongoDB
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pgt6r.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function connect() {
+    await client.connect();
+    console.log("Connected to MongoDB");
+}
+connect().catch(console.dir);
 
 app.get('/', (req, res) => res.send('Hello from Aesthetic Ware House!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
